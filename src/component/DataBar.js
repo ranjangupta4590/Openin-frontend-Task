1f3d5c2e09dd50f6402801f162ad6448144e20fe
ranjan_4590 <ranjangupta35558@gmail.com>
2023-10-20 19:16:05 +0530

    aling

--- src/component/DataBar.js
+++ src/component/DataBar.js
@@ -8,13 +8,14 @@ const DataBar = () => {
     return (
         <div className='grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-3'>
 
-            <div id="rev-box" className="data ">
-                <img src={revenue} width={35} height={35} id="card-icon" alt="revenue" />
+            <div id="rev-box" className="data">
+                <img src={revenue} width={45} height={45} id="card-icon" alt="revenue" />
                 <div className="mt-2 ">
-                    <p className="text-sm">Total Revenues</p>
+                    <p className="text-sm text-left ">Total Revenues</p>
+                    
                     <div className="flex justify-between mt-1">
                         <h2 className="text-xl font-bold">$ 2,129,430</h2>
-                        <button className="bg-[#E9F9EB] rounded-md p-1 text-xs font-semibold text-[#3CC952]">
+                        <button className="bg-[#E9F9EB] rounded-xl p-1 text-xs font-semibold text-[#3CC952]">
                             +2.5%
                         </button>
                     </div>
@@ -25,10 +26,10 @@ const DataBar = () => {
             <div id="trans-box" className="data ">
                 <img src={transaction} width={35} height={35} id="card-icon" alt="trans" />
                 <div className="mt-2 ">
-                    <p className="text-sm">Total Transaction</p>
+                    <p className="text-sm text-left">Total Transaction</p>
                     <div className="flex justify-between mt-1">
                         <h2 className="text-xl font-bold">1,520</h2>
-                        <button className="bg-[#E9F9EB] rounded-md p-1 text-xs font-semibold text-[#3CC952]">
+                        <button className="bg-[#E9F9EB] rounded-xl p-1 text-xs font-semibold text-[#3CC952]">
                             +1.7%
                         </button>
                     </div>
@@ -39,10 +40,10 @@ const DataBar = () => {
             <div id="like-box" className="data ">
                 <img src={likes} width={35} height={35} id="card-icon" alt="like" />
                 <div className="mt-2 ">
-                    <p className="text-sm">Total Likes</p>
+                    <p className="text-sm text-left">Total Likes</p>
                     <div className="flex justify-between mt-1">
                         <h2 className="text-xl font-bold">9,721</h2>
-                        <button className="bg-[#E9F9EB] rounded-md p-1 text-xs font-semibold text-[#3CC952]">
+                        <button className="bg-[#E9F9EB] rounded-xl p-1 text-xs font-semibold text-[#3CC952]">
                             +1.4%
                         </button>
                     </div>
@@ -53,10 +54,10 @@ const DataBar = () => {
             <div id="user-box" className="data ">
                 <img src={users} width={35} height={35} id="card-icon" alt="user" />
                 <div className="mt-2 ">
-                    <p className="text-sm">Total Users</p>
+                    <p className="text-sm text-left">Total Users</p>
                     <div className="flex justify-between mt-1">
                         <h2 className="text-xl font-bold">9,721</h2>
-                        <button className="bg-[#E9F9EB] rounded-md p-1 text-xs font-semibold text-[#3CC952]">
+                        <button className="bg-[#E9F9EB] rounded-xl p-1 text-xs font-semibold text-[#3CC952]">
                             +4.2%
                         </button>
                     </div>
